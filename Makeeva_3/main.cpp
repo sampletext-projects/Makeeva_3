71cf990600f380bf476e320b3bfd35e65fca0d5f
sampletext32 <sampletext32@bk.ru>
2020-06-16 20:17:26 +0300

    Add blockscheme and otchet

--- Makeeva_3/main.cpp
+++ Makeeva_3/main.cpp
@@ -8,39 +8,46 @@ int main()
 	cout << "Программу сделала Гузь Ева Евгеньевна УМЛ - 111\n";
 	cout << "Программа для проверки таблицы умножения\n";
 
-	int n;
-	cout << "Введите N: ";
-	cin >> n;
-
-	int correct = 0;
-	for (int i = 0; i < 10; i++)
+	int k;
+	do
 	{
-		cout << n << " * " << (i + 1) << " = ";
-		int t;
-		cin >> t;
-		if (t == n * (i + 1))
+		int n;
+		cout << "Введите N: ";
+		cin >> n;
+
+		int correct = 0;
+		for (int i = 0; i < 10; i++)
 		{
-			correct++;
+			cout << n << " * " << (i + 1) << " = ";
+			int t;
+			cin >> t;
+			if (t == n * (i + 1))
+			{
+				correct++;
+			}
 		}
-	}
 
-	cout << "Правильных ответов: " << correct << "; \n" << "Оценка: ";
-	if (correct == 10)
-	{
-		cout << "Отлично!\n";
+		cout << "Правильных ответов: " << correct << "; \n" << "Оценка: ";
+		if (correct == 10)
+		{
+			cout << "Отлично!\n";
+		}
+		else if (correct == 9)
+		{
+			cout << "Хорошо!\n";
+		}
+		else if (correct == 8)
+		{
+			cout << "Удовлетворительно!\n";
+		}
+		else
+		{
+			cout << "Неудовлетворительно!\n";
+		}
+		cout << "Повторить ввод(1): ";
+		cin >> k;
 	}
-	else if (correct == 9)
-	{
-		cout << "Хорошо!\n";
-	}
-	else if (correct == 8)
-	{
-		cout << "Удовлетворительно!\n";
-	}
-	else
-	{
-		cout << "Неудовлетворительно!\n";
-	}
+	while (k == 1);
 
 	system("pause");
 
